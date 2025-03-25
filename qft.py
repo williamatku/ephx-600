c741e332262536a2083e76ff1eddc9aa505b6058
morriswa <william@morriswa.org>
2025-03-25 13:40:01 -0500

    # hello QFT world!

---
+++ qft.py
@@ -0,0 +1,21 @@
+
+"""
+    QFT module
+
+    author: William Morris [morriswa/williamatku]
+"""
+
+import numpy as np
+import pennylane as qml
+
+
+def qft(qubits):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
