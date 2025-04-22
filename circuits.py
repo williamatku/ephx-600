1fae89150ee1970db7c89126bcc240d40152be08
morriswa <william@morriswa.org>
2025-04-21 22:47:53 -0500

    # working

--- circuits.py
+++ circuits.py
@@ -11,31 +11,36 @@ from numpy import pi
 import pennylane as qml
 
 
-def prepare_state_freq(freq, control_wire, wires):
+dev = qml.device("default.qubit")
+
+
+
+def pstate(freqs, control_wire, wires):
     """ adapted from https://pennylane.ai/qml/demos/tutorial_qft """
 
     qml.PauliX(wires=control_wire)
+
+    embed = None
+    for freq in freqs:
+        embed = qml.PhaseShift(2 * pi * freq, wires=control_wire)
+
     for wire in wires:
         qml.Hadamard(wires=wire)
-    qml.ControlledSequence(qml.PhaseShift(2 * pi * freq, wires=control_wire), control=wires)
+    qml.ControlledSequence(embed, control=wires)
+
     qml.PauliX(wires=control_wire)
 
 
-dev = qml.device("default.qubit")
 @qml.qnode(dev)
-def circuit_state(freq, control_wire, wires):
+def circuit_state(freqs, control_wire, wires):
     """ adapted from https://pennylane.ai/qml/demos/tutorial_qft """
-
-    for f in freq:
-        prepare_state_freq(freq=f, control_wire=control_wire, wires=wires)
-
+    pstate(freqs, control_wire, wires)
     return qml.state()
 
-@qml.qnode(dev)
-def circuit_qft_probs(freq, control_wire, wires):
-    """ adapted from https://pennylane.ai/qml/demos/tutorial_qft """
 
-    for f in freq:
-        prepare_state_freq(freq=f, control_wire=control_wire, wires=wires)
+@qml.qnode(dev)
+def circuit_qft_probs(freqs, control_wire, wires):
+    """ adapted from https://pennylane.ai/qml/demos/tutorial_qft """
+    pstate(freqs, control_wire, wires)
     qml.QFT(wires=wires)
     return qml.probs(wires=wires)
--- qft.py
+++ qft.py
@@ -9,16 +9,19 @@
 
 
 import matplotlib.pyplot as plt
-import numpy as np
 
 from circuits import circuit_state, circuit_qft_probs
 
 
-QUBITS = 8
+QUBITS = 6
 CONTROL_QUBIT = 0
 ENC_QUBITS = range(1, QUBITS+1)
 # only supports frequencies < 1
-FREQUENCIES = [1/49, 1/25]
+FREQUENCY_RES = 2
+FREQUENCY_BINS = [s / 10 ** FREQUENCY_RES for s in range(1, 10 ** FREQUENCY_RES)]
+print(len(FREQUENCY_BINS))
+# must be in frequency bin...
+FREQUENCIES = [31/100, 32/100, 71/100, 64/100]
 Q_STATES = 2**QUBITS
 
 
@@ -37,14 +40,35 @@ def show_probability_dist():
 
     state = circuit_qft_probs(FREQUENCIES, CONTROL_QUBIT, ENC_QUBITS)[:Q_STATES]
 
-    top_indices = sorted(range(len(state)), key=lambda i: state[i], reverse=True)[:len(FREQUENCIES)]
+    top_indices = sorted(range(len(state)), key=lambda i: state[i], reverse=True)
     top_values = [(index, state[index]) for index in top_indices]
 
+    reported = []
     for i, s in top_values:
-        print(f'{int(s*100)}% certain freq {1 - i/(2**QUBITS)} is present...')
+        freq = 1 - i/(2**QUBITS)
+        for f in FREQUENCY_BINS:
+            if round(freq, FREQUENCY_RES) == f:
+                reported.append(f)
+                break
+
+    found = []
+    others = []
+    for reported_freq in reported:
+
+        cat = False
+        for desired_freq in FREQUENCIES:
+            if reported_freq == desired_freq:
+                found.append(reported_freq)
+                cat = True
+
+        if not cat:
+            others.append(reported_freq)
+
+    found = set(found)
+    print(f"score: {len(found)/len(FREQUENCIES)}")
+    print(f"identified {len(found)} of {len(FREQUENCIES)} frequencies")
 
     plt.bar(range(len(state)), state)
     plt.xlabel("|x⟩")
     plt.ylabel("probs")
     plt.show()
-
