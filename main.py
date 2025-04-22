57e832fc8f4e40e43bdc4305947b2d33f2e18a83
morriswa <william@morriswa.org>
2025-04-22 00:08:57 -0500

    trials should probably start at 1 to make the graph look right

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from numpy.exceptions import ComplexWarning
 
 from qft import QFT
 
-TRIALS = 10
+TRIALS = 20
 FREQUENCY_RESOLUTION = 2
 MIN_QUBITS = 2
 MAX_QUBITS = 16
@@ -26,8 +26,8 @@ def main():
                     freqs.append(random.randint(1, 10**FREQUENCY_RESOLUTION)/(10**FREQUENCY_RESOLUTION))
                 qft.set_encoded_frequencies(freqs)
                 score, conf = qft.prep_state_apply_qft()
-                print(f"trial no: {trial_no}, n_qubits: {n_qubits}, enc_freqs: {enc_freqs}, score: {score}, average confidence: {conf}")
-                scoresheet.append([trial_no, n_qubits, enc_freqs, score, conf])
+                print(f"trial no: {trial_no+1}, n_qubits: {n_qubits}, enc_freqs: {enc_freqs}, score: {score}, average confidence: {conf}")
+                scoresheet.append([trial_no+1, n_qubits, enc_freqs, score, conf])
 
     with open('out.csv', 'w', newline='') as f:
         w = csv.writer(f, delimiter=',')
